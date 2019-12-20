2aa8543251fc215bc49649765ae87c673d849d18
safari <rsafari.s@gmail.com>
2019-12-20 14:28:19 -0600

    implemented mailer  for emails

--- lokialerts/container.py
+++ lokialerts/container.py
@@ -4,6 +4,7 @@ from tinydb import TinyDB
 from pathlib import Path
 
 from lokialerts.servicenode import ServiceNodeTable, ServiceNodeDB, inquirer
+from lokialerts.mailer import Mailer
 
 app_path = '%s/.lokialerts' % Path.home()
 data_path = app_path + '/data.json'
@@ -11,6 +12,13 @@ data_path = app_path + '/data.json'
 if not os.path.exists(app_path):
     os.makedirs(app_path)
 db = TinyDB(data_path)
+
+
+mailer = Mailer(
+    os.getenv('MAILER_USER'),
+    os.getenv('MAILER_PASSWORD'),
+    os.getenv('MAILER_RECIPIENTS')
+)
 
 service_node_db = ServiceNodeDB(db)
 service_node_table = ServiceNodeTable()
